a4c0bacf78caa59770afa7d0acff7bd5d1c85b0d
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-08-12 12:30:48 +0100

    feat(Dashboard): added initial tabs

--- laravel/resources/js/Pages/Dashboard.jsx
+++ laravel/resources/js/Pages/Dashboard.jsx
@@ -10,12 +10,58 @@ export default function Dashboard({ auth }) {
             <Head title="Dashboard" />
 
             <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!</div>
 
-                        <div classname="p-6 text-gray-900">
+                        
+                    </div>
+                </div>
+                
+                <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
                             <h2>Condutores</h2>
+                        </div>
+                        <div className='px-9'>
+                            <h4>Condutores Ativos</h4>
+                        </div>
+
+                        <div className='px-9'>
+                            <h4>Condutores Disponíveis</h4>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <h2>Veículos</h2>
+                        </div>
+                        <div className='px-9'>
+                            <h4>Veículos Ativos</h4>
+                        </div>
+
+                        <div className='px-9'>
+                            <h4>Veículos Disponíveis</h4>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900">
+                            <h2>Viagens</h2>
+                        </div>
+                        <div className='px-9'>
+                            <h4>Viagens em Curso</h4>
+                        </div>
+
+                        <div className='px-9'>
+                            <h4>Viagens Agendadas</h4>
                         </div>
                     </div>
                 </div>
