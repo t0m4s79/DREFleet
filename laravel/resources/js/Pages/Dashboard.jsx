ed459be2fb6a0502029160038462ab7ce25e4603
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-08-12 12:15:28 +0100

    first commit to dev/frontend, test

--- laravel/resources/js/Pages/Dashboard.jsx
+++ laravel/resources/js/Pages/Dashboard.jsx
@@ -13,6 +13,10 @@ export default function Dashboard({ auth }) {
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                         <div className="p-6 text-gray-900">You're logged in!</div>
+
+                        <div classname="p-6 text-gray-900">
+                            <h2>Condutores</h2>
+                        </div>
                     </div>
                 </div>
             </div>
