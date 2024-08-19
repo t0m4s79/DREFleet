b1ccc545741c46fcbf54b6c0ad793e0b385151e3
ramartins02 <randrademartins02@gmail.com>
2024-08-19 12:18:07 +0100

    feat(vehicles): implemented edit vehicle logic

--- laravel/app/Http/Controllers/VehicleController.php
+++ laravel/app/Http/Controllers/VehicleController.php
@@ -45,8 +45,31 @@ class VehicleController extends Controller
         return redirect('/vehicles');
     }
 
-    public function editVehicle() {
+    public function editVehicle(Vehicle $vehicle, Request $request) {
+        $incomingFields = $request->validate([
+            'make' => 'required', 
+            'model' => 'required',
+            'license_plate' => 'required',
+            'heavy_vehicle' => 'required',
+            'wheelchair_adapted' => 'required',
+            'capacity' => 'required',
+            'fuel_consumption' => 'required',
+            'status_code' => 'required',
+            'current_month_fuel_requests' => 'required'
+        ]);
 
+        $incomingFields['make'] = strip_tags($incomingFields['make']);
+        $incomingFields['model'] = strip_tags($incomingFields['model']);
+        $incomingFields['license_plate'] = strip_tags($incomingFields['license_plate']);
+        $incomingFields['heavy_vehicle'] = strip_tags($incomingFields['heavy_vehicle']);
+        $incomingFields['wheelchair_adapted'] = strip_tags($incomingFields['wheelchair_adapted']);
+        $incomingFields['capacity'] = strip_tags($incomingFields['capacity']);
+        $incomingFields['fuel_consumption'] = strip_tags($incomingFields['fuel_consumption']);
+        $incomingFields['status_code'] = strip_tags($incomingFields['status_code']);
+        $incomingFields['current_month_fuel_requests'] = strip_tags($incomingFields['current_month_fuel_requests']);
+
+        $vehicle->update($incomingFields);
+        return redirect('/vehicles');
     }
 
     public function deleteVehicle() {
