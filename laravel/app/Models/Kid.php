1dd9cf971488a501983b608bd04fb08e6291d63a
ramartins02 <randrademartins02@gmail.com>
2024-08-20 16:00:57 +0100

    chore(places): created places model

--- laravel/app/Models/Kid.php
+++ laravel/app/Models/Kid.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Kid extends Model                 //TODO: relations with places
 {
@@ -18,4 +19,9 @@ class Kid extends Model                 //TODO: relations with places
         'phone',
         'email'
     ];
+
+    public function places(): HasMany 
+    {
+        return $this->hasMany(Place::class);
+    }
 }
