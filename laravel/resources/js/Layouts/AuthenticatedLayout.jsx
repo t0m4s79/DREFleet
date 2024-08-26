0067bb6be26c6be604e00cf38f1b3c1c2ec996c5
ramartins02 <randrademartins02@gmail.com>
2024-08-26 10:57:14 +0100

    chore(navbar): change users navlinks

--- laravel/resources/js/Layouts/AuthenticatedLayout.jsx
+++ laravel/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -29,9 +29,46 @@ export default function Authenticated({ user, header, children }) {
                                     Veículos
                                 </NavLink>
 
-                                <NavLink href={route('drivers.index')} active={route().current('drivers.index')}>
-                                    Condutores
-                                </NavLink>
+
+                                <div className="hidden sm:flex sm:items-center sm:ms-6">
+                                    <div className="relative">                                
+                                        <Dropdown>
+                                            <Dropdown.Trigger>
+                                                <span className="inline-flex rounded-md">
+                                                    <button
+                                                        type="button"
+                                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
+                                                    >
+                                                        Utilizadores
+
+                                                        <svg
+                                                            className="ms-2 -me-0.5 h-4 w-4"
+                                                            xmlns="http://www.w3.org/2000/svg"
+                                                            viewBox="0 0 20 20"
+                                                            fill="currentColor"
+                                                        >
+                                                            <path
+                                                                fillRule="evenodd"
+                                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                                clipRule="evenodd"
+                                                            />
+                                                        </svg>
+                                                    </button>
+                                                </span>
+                                            </Dropdown.Trigger>
+
+                                            <Dropdown.Content>
+                                                <Dropdown.Link  href={route('drivers.index')} active={route().current('drivers.index')}>Condutores</Dropdown.Link>
+                                                {/* TODO: Links para: */}
+                                                {/* 1º) TODOS OS UTILIZADORES (SO PARTES DA TABELA USERS) */}
+                                                {/* 2º) ADMINISTRADORES */}
+                                                {/* 3º) GESTORES */}
+                                                {/* 4º) TECNICOS */}
+                                                {/* 5º) POR ATRIBUIR */}
+                                            </Dropdown.Content>
+                                        </Dropdown>
+                                    </div>
+                                </div>
 
                                 <NavLink href={route('kids.index')} active={route().current('kids.index')}>
                                     Crianças
