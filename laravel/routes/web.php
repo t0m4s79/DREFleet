ee8c166163b690ee1c5ab54f8409a67777b7821f
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-08-27 16:00:14 +0100

    refactor(drivers): changed edit route from post to put, changed controller to add message after editing driver

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -38,7 +38,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/drivers/create', [DriverController::class, 'showCreateDriverForm'])->name('drivers.create');
     Route::post('/drivers/create', [DriverController::class, 'createDriver'])->name('drivers.create');              //CREATE
     Route::get('/drivers/edit/{driver}', [DriverController::class, 'showEditScreen'])->name('drivers.showEdit');    //GET
-    Route::post('/drivers/edit/{driver}', [DriverController::class, 'editDriver'])->name('drivers.edit');           //EDIT
+    Route::put('/drivers/edit/{driver}', [DriverController::class, 'editDriver'])->name('drivers.edit');           //EDIT
     Route::delete('/drivers/delete/{driver}', [DriverController::class, 'deleteDriver'])->name('drivers.delete');     //DELETE
     
     //PROFILE
