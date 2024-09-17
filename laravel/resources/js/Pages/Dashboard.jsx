bed150e3062a316f3f8fab323cc6803ffc283004
Nessie360 <inesloja@edu.madeira.gov.pt>
2024-09-17 15:47:11 +0100

    added accordions to dashboard page

--- laravel/resources/js/Pages/Dashboard.jsx
+++ laravel/resources/js/Pages/Dashboard.jsx
@@ -21,12 +21,12 @@ export default function Dashboard({ auth }) {
                 </div>
                 
                 <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
-                    <div className="pb-6 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
                         <div className="p-6 text-gray-900">
-                            <h2>Condutores</h2>
+                            <h2 className='pb-3 text-lg font-bold'>Condutores</h2>
 
                             {/* TODO: Add drivers according to their status */}
-                            <Accordion>
+                            <Accordion style={{boxShadow: 'none'}}>
                                 <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel1-content"
@@ -35,21 +35,27 @@ export default function Dashboard({ auth }) {
                                 Condutores Ativos
                                 </AccordionSummary>
                                 <AccordionDetails>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
                                 </AccordionDetails>
                             </Accordion>
-                            <Accordion>
+                            <Accordion style={{boxShadow: 'none'}}>
                                 <AccordionSummary
                                 expandIcon={<ExpandMoreIcon />}
                                 aria-controls="panel2-content"
                                 id="panel2-header"
                                 >
-                                Condutores disponívies
+                                Condutores Disponíveis
                                 </AccordionSummary>
                                 <AccordionDetails>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-                                malesuada lacus ex, sit amet blandit leo lobortis eget.
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
                                 </AccordionDetails>
                             </Accordion>
                         </div>
@@ -58,37 +64,95 @@ export default function Dashboard({ auth }) {
 
                 </div>
 
-                {/* TODO: Change divs to use Accordion and add related information */}
                 <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
-                    <div className="pb-6 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
                         <div className="p-6 text-gray-900">
-                            <h2>Veículos</h2>
-                        </div>
-                        <div className='px-9'>
-                            <h4>Veículos Ativos</h4>
-                        </div>
+                            <h2 className='pb-3 text-lg font-bold'>Veículos</h2>
 
-                        <div className='px-9'>
-                            <h4>Veículos Disponíveis</h4>
+                            {/* TODO: Add drivers according to their status */}
+                            <Accordion style={{boxShadow: 'none'}}>
+                                <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                                >
+                                Veículos Ativos
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion style={{boxShadow: 'none'}}>
+                                <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                                >
+                                Veículos Disponíveis
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
+                                </AccordionDetails>
+                            </Accordion>
                         </div>
                     </div>
+
 
                 </div>
 
                 <div className="max-w-7xl mx-auto my-4 sm:px-6 lg:px-8">
-                    <div className="pb-6 bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg border-l-8 border-sky-600">
                         <div className="p-6 text-gray-900">
-                            <h2>Viagens</h2>
-                        </div>
-                        <div className='px-9'>
-                            <h4>Viagens em Curso</h4>
-                        </div>
+                            <h2 className='pb-3 text-lg font-bold'>Viagens</h2>
 
-                        <div className='px-9'>
-                            <h4>Viagens Agendadas</h4>
+                            {/* TODO: Add drivers according to their status */}
+                            <Accordion style={{boxShadow: 'none'}}>
+                                <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel1-content"
+                                id="panel1-header"
+                                >
+                                Viagens em Curso
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
+                                </AccordionDetails>
+                            </Accordion>
+                            <Accordion style={{boxShadow: 'none'}}>
+                                <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                                aria-controls="panel2-content"
+                                id="panel2-header"
+                                >
+                                Viagens Agendadas
+                                </AccordionSummary>
+                                <AccordionDetails>
+                                <ul className="px-4" style={{listStyleType: 'disc'}}>
+                                    <li>Coffee</li>
+                                    <li>Tea</li>
+                                    <li>Milk</li>
+                                </ul>
+                                </AccordionDetails>
+                            </Accordion>
                         </div>
                     </div>
+
+
                 </div>
+
+
             </div>
         </AuthenticatedLayout>
     );
