13e40bb541536b87560208d51ffdac3da664b200
Nessie360 <inesloja@edu.madeira.gov.pt>
2024-09-18 14:27:17 +0100

    added login logo to guest layout

--- laravel/resources/js/Layouts/GuestLayout.jsx
+++ laravel/resources/js/Layouts/GuestLayout.jsx
@@ -4,14 +4,18 @@ import { Link } from '@inertiajs/react';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-sky-100">
-            <div>
-                <Link href="/">
-                    <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                </Link>
-            </div>
-
+            
             <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                {children}
+                <div >
+                    <Link href="/">
+                        <img className="m-auto pt-8 pb-10 w-60 fill-current text-gray-500" src="/img/login_logo.svg" alt="App Login Logo" />
+                    </Link>
+                </div>
+
+                <div >
+                    {children}
+                </div>
+
             </div>
         </div>
     );
