9eeec9d97950be008694db56447ccbec703daebd
ramartins02 <randrademartins02@gmail.com>
2024-09-29 18:11:38 +0100

    test(order-stops): implemented unit tests

--- laravel/tests/Feature/OrderStopTest.php
+++ laravel/tests/Feature/OrderStopTest.php
@@ -14,7 +14,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 class OrderStopTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     protected $user;
 
     protected function setUp(): void
@@ -60,17 +60,112 @@ class OrderStopTest extends TestCase
 
 
     public function test_order_stop_creation(): void
-    {       
-        
+    {
+        $orderStopData = [
+            'planned_arrival_date' => now(),
+            'order_id' => Order::factory()->create()->id,
+            'place_id' => Place::factory()->create()->id,
+            'kid_id' => Kid::factory()->create()->id,
+        ];
+
+        $response = $this
+            ->actingAs($this->user)
+            ->post('/orderStops/create', $orderStopData);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/orders');
+
+        $this->assertDatabaseHas('order_stops', [
+            'planned_arrival_date' => $orderStopData['planned_arrival_date'],
+            'order_id' => $orderStopData['order_id'],
+            'place_id' => $orderStopData['place_id'],
+        ]);
+
+        $orderStop = OrderStop::where('order_id', $orderStopData['order_id'])
+                        ->where('place_id', $orderStopData['place_id'])
+                        ->where('planned_arrival_date', $orderStopData['planned_arrival_date'])
+                        ->first();
+
+
+        $this->assertDatabaseHas('kid_order_stop', [
+            'order_stop_id' => $orderStop->id,
+            'kid_id' => $orderStopData['kid_id'],
+            'place_id' => $orderStopData['place_id'],
+        ]);
     }
 
     public function test_order_stop_edit(): void
     {       
-        
+        $orderStop = OrderStop::factory()->create();
+
+        $this->assertDatabaseHas('order_stops', [
+            'planned_arrival_date' => null,
+        ]);
+
+        $updatedData = [
+            'planned_arrival_date' => now(),
+        ];
+
+        $response = $this
+            ->actingAs($this->user)
+            ->put("/orderStops/edit/{$orderStop->id}", $updatedData);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/orders');
+
+        $this->assertDatabaseHas('order_stops', [
+            'id' => $orderStop->id,
+            'planned_arrival_date' => $updatedData['planned_arrival_date'],
+        ]);
     }
 
     public function test_order_stop_deletion(): void
     {
+        $orderStop = OrderStop::factory()->create();
 
+        $this->assertDatabaseHas('order_stops', [
+            'id' => $orderStop->id,
+        ]);
+
+        $response = $this
+            ->actingAs($this->user)
+            ->delete("/orderStops/delete/{$orderStop->id}");
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/orders');
+
+        $this->assertDatabaseMissing('order_stops', [
+            'id' => $orderStop->id,
+        ]);
+
+    }
+
+    public function test_order_stop_reached(): void
+    {
+        $orderStop = OrderStop::factory()->create();
+
+        $this->assertDatabaseHas('order_stops', [
+            'actual_arrival_date' => null,
+        ]);
+
+        $updatedData = [
+            'actual_arrival_date' => now(),
+        ];
+
+        $response = $this
+            ->actingAs($this->user)
+            ->put("/orderStops/stopReached/{$orderStop->id}", $updatedData);
+
+        $response
+            ->assertSessionHasNoErrors()
+            ->assertRedirect('/orders');
+
+        $this->assertDatabaseHas('order_stops', [
+            'id' => $orderStop->id,
+            'actual_arrival_date' => $updatedData['actual_arrival_date'],
+        ]);
     }
 }
