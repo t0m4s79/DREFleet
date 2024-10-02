5c6cd54d488aba493ed4cf93720a398d777d47a6
ramartins02 <randrademartins02@gmail.com>
2024-10-02 16:05:56 +0100

    feat(vehicle-documents): started to implement vehicle documents logic, including migration files and factories

--- laravel/database/factories/VehicleFactory.php
+++ laravel/database/factories/VehicleFactory.php
@@ -2,8 +2,11 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Vehicle;
 use Illuminate\Support\Arr;
+use App\Models\VehicleDocument;
+use App\Models\VehicleAccessory;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Vehicle>
@@ -24,7 +27,7 @@ class VehicleFactory extends Factory
             'make' => Arr::random(['Ford','Reanult', 'VW', 'Fiat', 'Peugeot']),
             'model' => fake()->name(),
             'license_plate' => rand(111111,999999),
-            'year' => rand(1960,2024),
+            'year' => rand(1980,2024),
             'heavy_vehicle' => $heavyVehicle,
             'heavy_type' => $heavyType,
             'wheelchair_adapted' => fake()->boolean(),
@@ -36,5 +39,32 @@ class VehicleFactory extends Factory
             'fuel_type' => Arr::random(['Gasóleo','Gasolina 95','Gasolina 98','Híbrido','Elétrico']),
             'current_kilometrage' => rand(1,200000),
         ];
+    }
+
+    public function configure()
+    {
+        return $this->afterCreating(function (Vehicle $vehicle) {
+
+            VehicleDocument::factory()->create([
+                'vehicle_id' => $vehicle->id,
+                'name' => 'Seguro',
+            ]);
+
+            VehicleDocument::factory()->create([
+                'vehicle_id' => $vehicle->id,
+                'name' => 'Ficha de Inspeção',
+            ]);
+
+            VehicleDocument::factory()->create([
+                'vehicle_id' => $vehicle->id,
+                'name' => 'Seguro',
+            ]);
+
+            for ($i = 0; $i < rand(0, 3); $i++) {
+                VehicleAccessory::factory()->create([
+                    'vehicle_id' => $vehicle->id,
+                ]);
+            }
+        });
     }
 }
