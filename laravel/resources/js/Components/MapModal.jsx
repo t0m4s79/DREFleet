f8a1e889aa1109dec60fc5944742f1d7c20461c4
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-10-07 10:38:32 +0100

    fix(map): small fix to map modal

--- laravel/resources/js/Components/MapModal.jsx
+++ laravel/resources/js/Components/MapModal.jsx
@@ -10,8 +10,17 @@ export default function MapModal({ trajectory }) {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    let traject;
+    let routeArea;
+    let color;
     // useEffect(()=>{
-    const traject = JSON.parse(trajectory);
+    if (trajectory) {
+        traject = JSON.parse(trajectory);
+    }
+    if (route) {
+        routeArea = JSON.parse(route.area)
+        color = route.color
+    }
     //     console.log(traject)
     //     setSelectedRoute(traject)
     // }, trajectory)
@@ -27,7 +36,7 @@ export default function MapModal({ trajectory }) {
                 style={{ display:'flex', alignItems:'center', justifyContent:'center' }}
             >
                 <div style={{ top: '50%', margin: 'auto', width: '80%', height: '80%', backgroundColor: 'white', padding: '20px' }}>
-                    <LeafletMap routing={true} onTrajectoryChange={trajectory}/>
+                    <LeafletMap routing={false} onTrajectoryChange={trajectory} polygonCoordinates={routeArea} polygonColor={color}/>
                 </div>
 
             </Modal>
