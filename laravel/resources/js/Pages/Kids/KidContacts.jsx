2e17a37408ed6e40241bbc010aac94fc1a304b87
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-10-15 15:42:32 +0100

    feat(kid-contacts): added button to check a kid's contacts

--- laravel/resources/js/Pages/Kids/KidContacts.jsx
+++ laravel/resources/js/Pages/Kids/KidContacts.jsx
@@ -1,8 +1,20 @@
 import { Head } from '@inertiajs/react';
+import Table from '@/Components/Table';
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 
-export default function VehicleAccessoriesAndDocuments( {auth, kid} ) {
+export default function KidContacts( {auth, kid} ) {
 
     console.log(kid);
+    //Deconstruct data to display on "table"
+    const kidEmails = kid.emails.map((email) => {
+        return {
+            id: email.id
+        }
+    })
+
+    const EmailCols = {
+        id: 'ID',
+    }
     
     return (
         <AuthenticatedLayout
@@ -12,7 +24,12 @@ export default function VehicleAccessoriesAndDocuments( {auth, kid} ) {
 
             {<Head title='Contactos da Criança' />}
 
+            <div className="py-12 px-6">
+                <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
             {/*TODO: TABLES WITH DOCUMENTS AND ACCESSORIES */}
+                    <Table data={kidEmails} columnsLabel={EmailCols} editAction="" deleteAction="" dataId="id"/>
+                </div>
+            </div>
  
         </AuthenticatedLayout>
 
