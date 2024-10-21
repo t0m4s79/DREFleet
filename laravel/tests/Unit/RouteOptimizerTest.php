1e592180ff25085cb1ffb2bb9cb48ab9a3f3a362
ramartins02 <randrademartins02@gmail.com>
2024-10-21 15:45:20 +0100

    feat(route-optimizer): created a more complex test

--- laravel/tests/Unit/RouteOptimizerTest.php
+++ laravel/tests/Unit/RouteOptimizerTest.php
@@ -60,4 +60,30 @@ class RouteOptimizerTest extends TestCase
         $this->assertEquals($expectedPath, $result['path']);
         $this->assertEquals($expectedDistance, $result['totalDistance']);
     }
+
+    public function testPathWithMorePoints()
+    {
+        $distanceMatrix = [
+            [ 0,  2, 10, 20,  5, 13,  4,  8,  2],
+            [ 2,  0,  6, 12,  7,  9, 31,  2, 10],
+            [10,  6,  0,  8, 14,  6, 19, 13, 31],
+            [20, 12,  8,  0,  1, 12,  6,  9, 14],
+            [ 5,  7, 14,  1,  0,  8, 32, 13,  8],
+            [13,  9,  6, 12,  6,  0, 14,  3, 22],
+            [ 4, 31, 19,  6,  32, 14, 0,  7,  6],
+            [ 8,  2, 13,  9, 13,  3,  7,  0, 10],
+            [ 2, 10, 31, 14,  8, 22,  6, 10,  0],
+        ];
+        $placeIds = [101, 102, 103, 104, 105, 106, 107, 108, 109]; // Example place IDs
+
+        $optimizer = new RouteOptimizer($distanceMatrix, $placeIds);
+        $result = $optimizer->nearestNeighbor();
+
+        // Updated expected path and total distance
+        $expectedPath = [101, 102, 108, 106, 103, 104, 105, 107, 109]; 
+        $expectedDistance = 60; // 10 (101 to 102) + 20 (102 to 103) + 12 (103 to 104)
+
+        $this->assertEquals($expectedPath, $result['path']);
+        $this->assertEquals($expectedDistance, $result['totalDistance']);
+    }
 }
