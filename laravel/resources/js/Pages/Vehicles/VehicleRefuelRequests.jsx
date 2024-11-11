23a901548abdfa667ea2a1cbe24eaa86c6b8c7ac
Tomás Marcos <50717794+t0m4s79@users.noreply.github.com>
2024-11-11 15:14:11 +0000

    refactor(fuel-request): changed datagrid column definition, styled warning row

--- laravel/resources/js/Pages/Vehicles/VehicleRefuelRequests.jsx
+++ laravel/resources/js/Pages/Vehicles/VehicleRefuelRequests.jsx
@@ -1,11 +1,27 @@
 import Table from '@/Components/Table';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 import { Button, Alert, Snackbar } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import { useEffect, useState } from 'react';
+import { parse } from 'date-fns';
+import { DataGrid } from '@mui/x-data-grid';
+import { WarningAmber } from '@mui/icons-material';
 
-export default function VehicleKilometrageReports( {auth, vehicle, flash} ) {
+const isRequestExceptional = (n) => {
+    if(n > 6) {
+        return (
+            <div style={{ color: '#E8B012' }}>
+                <WarningAmber />
+                {n}
+            </div>
+        )
+    } else{
+        return n
+    }
+}
+
+export default function VehicleRefuelReports( {auth, vehicle, flash} ) {
     console.log(vehicle);
     const [openSnackbar, setOpenSnackbar] = useState(false);
     const [snackbarMessage, setSnackbarMessage] = useState('');
@@ -51,11 +67,113 @@ export default function VehicleKilometrageReports( {auth, vehicle, flash} ) {
         created_at: 'Data de Criação',
         updated_at: 'Data da Última Atualização',
     };
+
+    const vehicleRefuelColumns = [
+        {
+            field: 'id',
+            headerName: 'ID',
+            flex: 1,
+            maxWidth: 60,
+            hideable: false
+        },
+        {
+            field: 'date',
+            headerName: 'Data',
+            type: 'date',
+            flex: 1,
+            valueGetter: (params) => {
+                // const parsedDate = parse(params, 'dd-MM-yyyy', new Date());
+                const parsedDate = parse(params, 'yyyy-MM-dd', new Date());
+                return parsedDate
+            },
+            hideable: false
+        },
+        {
+            field: 'kilometrage',
+            headerName: 'Kilometragem',
+            flex: 1,
+            disableColumnMenu: true,
+        },
+        {
+            field: 'quantity',
+            headerName: 'Quantidade depositada',
+            flex: 1,
+            disableColumnMenu: true,
+        },
+        {
+            field: 'cost_per_unit',
+            headerName: 'Custo por unidade',
+            flex: 1,
+            disableColumnMenu: true,
+        },
+        {
+            field: 'total_cost',
+            headerName: 'Custo total',
+            flex: 1,
+            disableColumnMenu: true,
+        },
+        {
+            field: 'fuel_type',
+            headerName: 'Tipo de combustível',
+            flex: 1,
+            disableColumnMenu: true,
+            sortable: false,
+        },
+        {
+            field: 'monthly_request_number',
+            headerName: 'Número de pedido mensal',
+            flex: 1,
+            renderCell: (params) => (
+                isRequestExceptional(params.value)
+            )
+        },
+        {
+            field: 'request_type',
+            headerName: 'Tipo de pedido',
+            flex: 1,
+        },
+        {
+            field: 'vehicle_id',
+            headerName: 'Veículo',
+            flex: 1,
+            disableColumnMenu: true,
+            sortable: false,
+            maxWidth: 100,
+            renderCell: (params) => (
+                <Link href={route('vehicles.showEdit', params.value)}>
+                    <Button >{vehicle.license_plate}</Button>
+                </Link>
+            )
+        },
+        {
+            field: 'created_at',
+            headerName: 'Data de Criação',
+            type: 'dateTime',
+            flex: 1,
+            maxWidth: 180,
+            valueGetter: (params) => {
+                const parsedDate = parse(params, 'dd-MM-yyyy HH:mm:ss', new Date());
+                return parsedDate
+            },
+        },
+        {
+            field: 'updated_at',
+            headerName: 'Data da Última Atualização',
+            type: 'dateTime',
+            flex: 1,
+            maxWidth: 200,
+            valueGetter: (params) => {
+                const parsedDate = parse(params, 'dd-MM-yyyy HH:mm:ss', new Date());
+                return parsedDate
+            },
+        },
+        
+    ]
     
     return (
         <AuthenticatedLayout
             user={auth.user}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Pedidos de Reabastecimento do Veículo #{vehicle.id}</h2>}
+            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Pedidos de Reabastecimento do Veículo #{vehicle.id} - {vehicle.license_plate}</h2>}
         >
 
             {<Head title='Pedidos de Reabastecimento do Veículo' />}
@@ -80,6 +198,15 @@ export default function VehicleKilometrageReports( {auth, vehicle, flash} ) {
                                 editAction="vehicleRefuelRequests.showEdit"
                                 deleteAction="vehicleRefuelRequests.delete"
                                 dataId="id" // Ensure the correct field is passed for DataGrid's `id`
+                            />
+
+                            <DataGrid
+                                rows={vehicleRequests}
+                                columns={vehicleRefuelColumns}
+                                density='compact'
+                                getRowClassName={(params) => {
+                                    return params.row.monthly_request_number > 6 ? 'warning-row' : '';
+                                }}
                             />
                         </div>
                     </div>
