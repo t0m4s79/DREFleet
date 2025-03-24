e0f56ced7fb0d726d0dfaf1d430910e4915385d5
Nessie360 <inesloja@edu.madeira.gov.pt>
2025-03-24 13:05:59 +0000

    padding and margin corrections for dashboard components

--- laravel/resources/js/Components/Charts/MaintenanceRequestsChart.jsx
+++ laravel/resources/js/Components/Charts/MaintenanceRequestsChart.jsx
@@ -17,6 +17,7 @@ const Title = styled(Typography)(({ theme }) => ({
     color: theme.palette.text.primary,
     fontWeight: 600,
     marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(2),
 }));
 
 
@@ -39,7 +40,7 @@ const MaintenanceRequestsChart = ({ maintenanceRequests }) => {
                 Registos de Manutenção
             </Title>
 
-            <LineChart
+            <LineChart className="max-w-fit"
                 xAxis={[{
                     scaleType: 'point',
                     data: maintenanceRequestsData.map(d => d.month),
